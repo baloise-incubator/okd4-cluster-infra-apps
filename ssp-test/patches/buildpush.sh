88fa16c85379c52ac50c40097fcefa38f632eb7d
Nikolas Philips <nikolas.philips@gmail.com>
2022-10-03 23:39:12 +0200

    Prepare new cluster

--- ssp-test/patches/buildpush.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker build . -f Dockerfile.admin-ui -t artifactory-jcr.baloise.dev/usu/admin-ui:3.0.0
-docker push artifactory-jcr.baloise.dev/usu/admin-ui:3.0.0
-docker build . -f Dockerfile.client -t artifactory-jcr.baloise.dev/usu/client:2-4-1
-docker push artifactory-jcr.baloise.dev/usu/client:2-4-1
-docker build . -f Dockerfile.profile-ui -t artifactory-jcr.baloise.dev/usu/profile-ui:3.0.0
-docker push artifactory-jcr.baloise.dev/usu/profile-ui:3.0.0
